1136c516f333065e8e98c7ffb7437987341c7c7c
trexxet <atomnik07@gmail.com>
2025-01-12 02:52:21 +0500

    Add no-client (headless) mode to demo-01

--- demo/01-helloworld/main.cpp
+++ demo/01-helloworld/main.cpp
@@ -19,13 +19,16 @@ int main (int argv, char** args) {
 	Based::log.write (DEMO_NAME);
 
 	Based::Engine engine;
-	engine.enable_client();
+	if (ENABLE_CLIENT)
+		engine.enable_client();
 
 	Based::Lua::File conf (CONFIG_PATH, Based::Lua::BindTypes::Geometry);
-	Based::Size2D<int> windowSize = conf["window"]; 
-	engine.client->create_window (DEMO_NAME, windowSize);
+	if (ENABLE_CLIENT) {
+		Based::Size2D<int> windowSize = conf["window"]; 
+		engine.client->create_window (DEMO_NAME, windowSize);
+	}
 
-	SceneMain sceneMain (engine.client->window(), conf);
+	SceneMain sceneMain (ENABLE_CLIENT ? engine.client->window() : nullptr, conf);
 	engine.sceneManager.currentScene = &sceneMain;
 
 	Based::log.write ("Starting main loop");
